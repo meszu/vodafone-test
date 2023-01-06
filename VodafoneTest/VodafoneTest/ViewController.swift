13f2d76503513d208bae17bd83d22c0b5789d822
Mészáros Kristóf <meszaroskristof@Meszaros-MacBook-Air.local>
2023-01-06 21:17:29 +0100

    create vodafone-test project

---
+++ VodafoneTest/VodafoneTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VodafoneTest
+//
+//  Created by Mészáros Kristóf on 2023. 01. 06..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
